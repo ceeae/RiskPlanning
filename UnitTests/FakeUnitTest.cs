aa5e19fc73d05515797371ad7356d5b7d0037e96
ceeae <ceeaeprj@gmail.com>
2017-07-11 15:57:26 +0200

    first

---
+++ UnitTests/FakeUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace UnitTests
+{
+    public class FakeTest
+    {
+
+        [Fact]
+        public void AlwaysReturnTrue()
+        {
+            bool result = true;
+
+            result.Should().BeTrue();
+
+        }
+
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void ForgetScenarioAlwaysReturnTrue(int param)
+        {
+            bool result = true;
+
+            result.Should().BeTrue();
+
+        }
+
+
+    }
+}
